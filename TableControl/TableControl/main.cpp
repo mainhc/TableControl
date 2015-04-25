bced5408c0f235bd8e9edb1994c6b2d15d5074be
寒春 郭 <ghc1111@163.com>
2015-04-25 21:47:22 +0800

    Xcode工程

---
+++ TableControl/TableControl/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  TableControl
+//
+//  Created by 寒春 郭 on 15-4-25.
+//  Copyright (c) 2015年 寒春 郭. All rights reserved.
+//
+
+#include <iostream>
+#include "tinyxml2.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+  
+    return 0;
+}
